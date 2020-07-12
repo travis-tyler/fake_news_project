59caa06408aeb4eb4662180c970c19388ef9e8ae
hal-pemulis <travisatyler@gmail.com>
2020-07-12 17:47:33 -0500

    Fixed ML pipeline; added confidence to flask app

--- 06_Flask/app.py
+++ 06_Flask/app.py
@@ -3,21 +3,48 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 import joblib
 
-# Load model from disk
-loaded_pipeline = joblib.load('../05_Models/fake_title_SVM_model.sav')
+# Load model pipeline from disk
+loaded_pipeline = joblib.load('../05_Models/fake_title_SVM_pipeline.sav')
 loaded_pipeline
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('form.html')
-
-@app.route('/submit', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def submit():
-    text = request.form['text']
-    result = loaded_pipeline.predict([text])
-    return f'HEADLINE: {text}\n-------------------------------\nPREDICTION: This article is probably {str(result[0]).upper()}!'
+
+    template_data = {
+        'headline' : '',
+        'prediction': '',
+        'percent': '',
+        'prediction_color': 'black',
+        'percent_color': 'black'
+        }
+
+    if request.method == 'POST':
+        text = request.form['text']
+
+        template_data['headline'] = text
+
+        template_data['prediction'] = str(loaded_pipeline.predict([text])[0])
+        if template_data['prediction'] == 'True':
+            template_data['percent'] = round(loaded_pipeline.predict_proba([text])[0][1] * 100, 2)
+            template_data['prediction_color'] = 'green'
+        else:
+            template_data['percent'] = round(loaded_pipeline.predict_proba([text])[0][0] * 100, 2)
+            template_data['prediction_color'] = 'red'
+
+        if template_data['percent'] <= 50:
+            template_data['percent_color'] = 'red'
+        elif template_data['percent'] < 60:
+            template_data['percent_color'] = 'orange'
+        elif template_data['percent'] < 75:
+            template_data['percent_color'] = 'yellow'
+        elif template_data['percent'] <= 90:
+            template_data['percent_color'] = 'green'         
+        else:
+            template_data['percent_color'] = 'blue'
+
+    return render_template('form.html', **template_data)
 
 if __name__=='__main__':
     app.run(debug=True)
