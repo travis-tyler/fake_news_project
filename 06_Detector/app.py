3a51645d93f6f5ccb996ec87721c3c8c6a904df1
hal-pemulis <travisatyler@gmail.com>
2020-07-21 17:04:58 -0500

    Cleaned larger dataset and made better models; added text to website

--- 06_Detector/app.py
+++ 06_Detector/app.py
@@ -2,11 +2,11 @@ from flask import Flask, request, render_template
 import joblib
 
 # Load model pipeline from disk
-title_pipeline = joblib.load('05_Models/fake_title_SVM_pipeline.sav')
-title_pipeline2 = joblib.load('05_Models/combined_title_SVM_linear_pipeline.sav')
+title_pipeline = joblib.load('../05_Models/large_title_SVM_pipeline.sav')
+title_pipeline2 = joblib.load('../05_Models/small_title_SVM_pipeline.sav')
 
-body_pipeline = joblib.load('05_Models/fake_body_SVM_pipeline.sav')
-body_pipeline2 = joblib.load('05_Models/combined_body_PAC_pipeline.sav')
+body_pipeline = joblib.load('../05_Models/large_body_SVM_pipeline.sav')
+body_pipeline2 = joblib.load('../05_Models/small_body_SVM_pipeline.sav')
 
 
 app = Flask(__name__)
@@ -44,21 +44,20 @@ def prediction():
             'prediction_color': 'black',
             'percent_color': 'black'
         }
-
     }
 
     if request.method == 'POST':
         text = request.form['text']
 
-        template_data['headline'] = text
+        template_data['headline'] = text.lower()
 
         # First model
-        template_data['pipeline1']['prediction'] = str(title_pipeline.predict([text])[0])
+        template_data['pipeline1']['prediction'] = str(title_pipeline.predict([text.lower()])[0])
         if template_data['pipeline1']['prediction'] == 'True':
-            template_data['pipeline1']['percent'] = round(title_pipeline.predict_proba([text])[0][1] * 100, 2)
+            template_data['pipeline1']['percent'] = round(title_pipeline.predict_proba([text.lower()])[0][1] * 100, 2)
             template_data['pipeline1']['prediction_color'] = 'green'
         else:
-            template_data['pipeline1']['percent'] = round(title_pipeline.predict_proba([text])[0][0] * 100, 2)
+            template_data['pipeline1']['percent'] = round(title_pipeline.predict_proba([text.lower()])[0][0] * 100, 2)
             template_data['pipeline1']['prediction_color'] = 'red'
 
         if template_data['pipeline1']['percent'] < 60:
@@ -106,8 +105,12 @@ def predict_body():
             'percent_color': 'black'
         },
         'pipeline2': {
-            'prediction': '',}
+            'prediction': '',
+            'percent': '',
+            'prediction_color': 'black',
+            'percent_color': 'black'
         }
+    }
 
 
     if request.method == 'POST':
@@ -115,12 +118,13 @@ def predict_body():
 
         template_data['headline'] = text
 
-        template_data['pipeline1']['prediction'] = str(body_pipeline.predict([text])[0])
+        # First model
+        template_data['pipeline1']['prediction'] = str(body_pipeline.predict([text.lower()])[0])
         if template_data['pipeline1']['prediction'] == 'True':
-            template_data['pipeline1']['percent'] = round(body_pipeline.predict_proba([text])[0][1] * 100, 2)
+            template_data['pipeline1']['percent'] = round(body_pipeline.predict_proba([text.lower()])[0][1] * 100, 2)
             template_data['pipeline1']['prediction_color'] = 'green'
         else:
-            template_data['pipeline1']['percent'] = round(body_pipeline.predict_proba([text])[0][0] * 100, 2)
+            template_data['pipeline1']['percent'] = round(body_pipeline.predict_proba([text.lower()])[0][0] * 100, 2)
             template_data['pipeline1']['prediction_color'] = 'red'
 
         if template_data['pipeline1']['percent'] < 60:
@@ -138,9 +142,22 @@ def predict_body():
         # Second model
         template_data['pipeline2']['prediction'] = str(body_pipeline2.predict([text])[0])
         if template_data['pipeline2']['prediction'] == 'True':
+            template_data['pipeline2']['percent'] = round(body_pipeline2.predict_proba([text])[0][1] * 100, 2)
             template_data['pipeline2']['prediction_color'] = 'green'
         else:
+            template_data['pipeline2']['percent'] = round(body_pipeline2.predict_proba([text])[0][0] * 100, 2)
             template_data['pipeline2']['prediction_color'] = 'red'
+
+        if template_data['pipeline2']['percent'] < 60:
+            template_data['pipeline2']['percent_color'] = 'red'
+        elif template_data['pipeline2']['percent'] < 70:
+            template_data['pipeline2']['percent_color'] = 'orange'
+        elif template_data['pipeline2']['percent'] < 80:
+            template_data['pipeline2']['percent_color'] = 'yellow'
+        elif template_data['pipeline2']['percent'] <= 90:
+            template_data['pipeline2']['percent_color'] = 'green'         
+        else:
+            template_data['pipeline2']['percent_color'] = 'blue'
 
     return render_template('Prediction2.html', **template_data)
 
