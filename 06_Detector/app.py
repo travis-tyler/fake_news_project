ca78d2f57346cc42184d6047604df0a5a7ad68e8
travis-tyler <travisatyler@gmail.com>
2021-10-05 09:42:25 -0500

    added new page with pretrained model for baseline

--- 06_Detector/app.py
+++ 06_Detector/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import joblib
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 # Load model pipeline from disk
 title_pipeline = joblib.load('05_Models/large_title_SVM_pipeline.sav')
@@ -160,6 +161,42 @@ def predict_body():
             template_data['pipeline2']['percent_color'] = 'blue'
 
     return render_template('Prediction2.html', **template_data)
+
+@app.route('/facehug_baseline', methods=['GET', 'POST'])
+def facehug():
+    tokenizer = AutoTokenizer.from_pretrained("ghanashyamvtatti/roberta-fake-news")
+    model = AutoModelForSequenceClassification.from_pretrained("ghanashyamvtatti/roberta-fake-news")
+    template_data = {'text':'', 'pred':'', 'prob':'', 'prediction_color':'', 'percent_color':''}
+    if request.method == 'POST':
+        text = request.form['text']
+
+        inputs = tokenizer(text, return_tensors='pt')
+        output = model(**inputs)
+
+        pred_logits = output.logits.softmax(dim=1).detach().cpu().flatten().numpy().tolist()
+        prob = round(max(pred_logits), 2)
+        template_data['prob'] = prob
+        pred = pred_logits.index(prob)
+
+        if pred == 0:
+            template_data['pred'] = 'False'
+            template_data['prediction_color'] = 'Red'
+        else:
+            template_data['pred'] = 'True'
+            template_data['prediction_color'] = 'Green'
+
+        if template_data['prob'] < .60:
+            template_data['percent_color'] = 'red'
+        elif template_data['prob'] < .70:
+            template_data['percent_color'] = 'orange'
+        elif template_data['prob'] < .80:
+            template_data['percent_color'] = 'yellow'
+        elif template_data['prob'] <= .90:
+            template_data['percent_color'] = 'green'         
+        else:
+            template_data['percent_color'] = 'blue'
+
+    return render_template('facehug.html', **template_data)
 
 if __name__=='__main__':
     app.run(debug=True)
